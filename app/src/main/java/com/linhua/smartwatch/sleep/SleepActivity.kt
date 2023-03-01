d6f99caca5811ca2d20139933b267e2638fa0fe6
sherwinstar <slzhouzsl@163.com>
2023-03-01 17:08:14 +0800

    update heart rate ui

--- app/src/main/java/com/linhua/smartwatch/sleep/SleepActivity.kt
+++ app/src/main/java/com/linhua/smartwatch/sleep/SleepActivity.kt
@@ -1,20 +1,21 @@
-package com.linhua.smartwatch.heartrate
+package com.linhua.smartwatch.sleep
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.highlight.Highlight
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener
 import com.linhua.smartwatch.R
 import com.linhua.smartwatch.base.BaseActivity
-import com.linhua.smartwatch.chart.DemoBase
 
-class HeartRateActivity : BaseActivity(), OnChartValueSelectedListener {
+class SleepActivity : BaseActivity(), OnChartValueSelectedListener {
 
     override fun initData() {
 
     }
 
     override fun getLayoutId(): Int {
-        return R.layout.activity_heart_rate
+        return R.layout.activity_sleep
     }
 
     override fun onListener() {
